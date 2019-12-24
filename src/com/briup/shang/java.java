f3f617336272c0e7075e9a7592862567096df099
xzl670327249 <670327249@qq.com>
2019-12-24 15:08:39 +0800

    谢卓霖提交

--- src/com/briup/shang/java.java
+++ src/com/briup/shang/java.java
@@ -4,4 +4,9 @@ public class java {
 	public static void main(String[] args) {
 		System.out.println("this is first ");
 	}
+	
+	public void xixi() {
+		System.out.println("少女的悲伤烫伤了你的背，直击你的心脏");
+		
+	}
 }
